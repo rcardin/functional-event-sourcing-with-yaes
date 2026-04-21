e8c1604cf87b9058fc65b51644d2d31b591c82f2
rcardin <riccardo.cardin@gmail.com>
2026-04-21 12:48:09 +0200

    feat: Fixed compilation error

--- src/main/scala/in/rcard/fes/App.scala
+++ src/main/scala/in/rcard/fes/App.scala
@@ -2,13 +2,13 @@ package in.rcard.fes
 
 import in.rcard.fes.copy.application.RegisterCopyRoute
 import in.rcard.fes.copy.domain.usecase.{CopyIdGenerator, RegisterCopyUseCase}
-import in.rcard.yaes.http.server.YaesServer
+import in.rcard.yaes.http.server.{ServerDef, YaesServer}
 import in.rcard.yaes.slf4j.Slf4jLog
-import in.rcard.yaes.{Reader, Shutdown, YaesApp}
+import in.rcard.yaes.{Clock, Input, Output, Random, Reader, Shutdown, Sync, System, YaesApp}
 
 class App extends YaesApp {
 
-  override def run: Unit = {
+  override def run: (Sync, Output, Input, Random, Clock, System) ?=> Unit = {
     Slf4jLog.run {
       Shutdown.run {
         server().run(port = 8080)
@@ -16,7 +16,7 @@ class App extends YaesApp {
     }
   }
 
-  private def server() = {
+  private def server(): ServerDef = {
     Reader.run(CopyIdGenerator.live) {
       Reader.run(RegisterCopyUseCase.live) {
         YaesServer.route(
