02915d1faf8a94e5e00c27f15f29fe58661de0d4
rcardin <riccardo.cardin@gmail.com>
2026-05-07 16:16:32 +0200

    feat: WIP

--- src/main/scala/in/rcard/fes/copy/domain/port/FindCopyByIsbnPort.scala
+++ src/main/scala/in/rcard/fes/copy/domain/port/FindCopyByIsbnPort.scala
@@ -4,8 +4,8 @@ import in.rcard.fes.copy.domain.Domain
 import in.rcard.fes.copy.domain.Domain.ISBN
 import in.rcard.fes.copy.domain.port.FindCopyByIsbnPort.Error
 import in.rcard.fes.copy.domain.usecase.RegisterCopyUseCase.CopyToRegister
-import in.rcard.fes.utils.reader
 import in.rcard.yaes.Reader.read
+import in.rcard.yaes.Reader.reader
 import in.rcard.yaes.http.client.{HttpRequest, Uri, YaesClient}
 import in.rcard.yaes.{Reader, raises, reads}
 
